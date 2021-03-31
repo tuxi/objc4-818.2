dee20983b16126a8cf5a29bc010e062b2eb84d2d
tuxi <seyooe@gmail.com>
2021-03-31 16:14:18 +0800

    First Commit

---
+++ objc_debug/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  objc_debug
+//
+//  Created by cooci on 2021/1/5.
+
+
+// KC 重磅提示 调试工程很重要 源码直观就是爽
+// ⚠️编译调试不能过: 请你检查以下几小点⚠️
+// ①: enable hardened runtime -> NO
+// ②: build phase -> denpendenice -> objc
+// 爽了之后,还来一个 👍
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        NSObject *objc = [NSObject alloc];
+        NSLog(@"Hello, World!");
+        
+        __weak NSObject *obj = objc;
+        NSLog(@"%@", obj);
+    }
+    return 0;
+}
